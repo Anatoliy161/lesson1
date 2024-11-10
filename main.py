f61f019fa9098d62b0f897bfa54a6c9b604c8e0d
Anatoliy161 <aa-anatolijtarasov@yandex.ru>
2024-11-10 16:46:21 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#1st program
-print((9 ** 0.5) * 5)
-#2nd program
-print(9.99 > 9.98 and 1000 != 1000.1 )
-#3rd program
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-#4th program
-print(int(float(('123.456')) * 10) % 10)
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net")) not in (recipient or sender):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    elif sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+send_email('messagje', 'university.help@gmail.com', '@.com')
+send_email('messagje', 'university.help@gmail.com')
+send_email('messagje', 'universi@.com')
+send_email('messagje', 'universi@.co')
+send_email('messagje', 'universi.com')
